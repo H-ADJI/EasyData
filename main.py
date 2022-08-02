d6e40c8bb2362a6982747f451a44cfaccd4f71bc
hunter <khalilhad9@gmail.com>
2022-08-02 17:20:10 +0100

    created entry point to  test core + parallelism usign pages

--- main.py
+++ main.py
@@ -6,23 +6,31 @@ Author: KHALIL HADJI
 Copyright:  HENCEFORTH 2022
 '''
 import asyncio
-from playwright.async_api import async_playwright, Playwright
-from nsa.core.execute import scrape
+from datetime import datetime
+from playwright.async_api import async_playwright, Playwright, Browser, BrowserContext
+from nsa.core.execute import GeneralPurposeScraper
 import aiofiles
 import json
+import random
 
 
-async def run(playwright: Playwright):
-
-    chromium = playwright.chromium  # or "firefox" or "webkit".
-    browser = await chromium.launch(headless=False)
-    page = await browser.new_page()
-    data = await scrape(using=page, website="hespress", objectives=["article_links"], user_data={"categories_list": ["مجتمع", "اقتصاد"]})
-    async with aiofiles.open("./nsa/database/hespress.json", "w") as f:
-        await f.write(json.dumps(data))
+async def nsa(context: BrowserContext, objective):
+    # TODO: try handle parallelism using contexts
+    page = await context.new_page()
+    data = await GeneralPurposeScraper.scrape(using=page, website="hespress", objective=objective, user_data={"categories_list": ["سياسة", "مجتمع", "اقتصاد"]})
+    async with aiofiles.open(f"./nsa/database/hespress_{datetime.now().isoformat(timespec='seconds')}.json", "w") as f:
+        await f.write(json.dumps(data, ensure_ascii=False))
 
 
 async def main():
+    tasks = []
     async with async_playwright() as playwright:
-        await run(playwright)
+        chromium = playwright.chromium  # or "firefox" or "webkit".
+        browser = await chromium.launch(headless=False)
+        context = await browser.new_context()
+        my_objectives = ["article_links", "most_viewed", "test_do_until"]
+        for obj in my_objectives:
+            tasks.append(asyncio.create_task(
+                nsa(context=context, objective=obj)))
+        await asyncio.gather(*tasks)
 asyncio.run(main())
