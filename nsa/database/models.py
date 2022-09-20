f4ef20ca444e386730449927250f734d0ea529f1
khalil <khalilhad9@gmail.com>
2022-09-20 15:09:18 +0100

    linked between collections

--- nsa/database/models.py
+++ nsa/database/models.py
@@ -7,25 +7,15 @@ Copyright:  HENCEFORTH 2022
 '''
 from datetime import datetime
 from fastapi_users.db import BeanieBaseUser
-from beanie import Document, PydanticObjectId
-from typing import List
+from beanie import Document, PydanticObjectId, Link
+from typing import List, Optional
 from nsa.models.project import Frequency
+from pydantic import BaseModel
 
 
 class User(BeanieBaseUser[PydanticObjectId]):
     first_name: str
     last_name: str
-
-
-class Project(Document):
-    description: str
-    title: str
-    tags: List[str]
-    image: str
-    owner: User
-
-    class Settings:
-        name = "projects"
 
 
 class Scraping_plan(Document):
@@ -42,14 +32,29 @@ class Scraping_job(Document):
     title: str
     schedule_date: datetime
     frequency: Frequency
-    scraping_plan: Scraping_plan
+    scraping_plan: Link[Scraping_plan]
 
     class Settings:
         name = "scraping_jobs"
 
 
+class Project(Document):
+    description: str
+    title: str
+    tags: List[str]
+    image: str
+    owner: Link[User]
+    jobs: Optional[List[Link[Scraping_job]]]
+
+    class Settings:
+        name = "projects"
+
+
 class Output_data(Document):
+    website: str
     data: dict
+    date: datetime
+    source_job: Link[Scraping_job]
 
     class Settings:
         name = "data_nd_analytics"
