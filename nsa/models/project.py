89411e670a5f03fbf2a7748f9676d3b9f242dad1
khalil <khalilhad9@gmail.com>
2022-09-21 11:25:57 +0100

    added projects pydantic models for creating, reading and updating endpoints

--- nsa/models/project.py
+++ nsa/models/project.py
@@ -7,35 +7,19 @@ Copyright:  HENCEFORTH 2022
 '''
 from datetime import datetime
 from pydantic import BaseModel
-from typing import List, Union
+from typing import List, Union, Optional
 
-
-class Project(BaseModel):
+class ProjectBase(BaseModel):
     description: str
     title: str
     tags: List[str]
     image: str
 
 
-class Scraping_plan(BaseModel):
-    plan: dict
+class Project_read(ProjectBase):
+    pass
 
 
-class Analytics(BaseModel):
-    plan: dict
-
-
-class Frequency(BaseModel):
-    weeks: int
-    days: int
-    hours: int
-    minutes: int
-    seconds: int
-    start_date: datetime
-    end_date: datetime
-    timezone: str
-
-
-class Date(BaseModel):
-    run_date: datetime
-    timezone: str
+class Project_update(ProjectBase):
+    __annotations__ = {k: Optional[v]
+                       for k, v in ProjectBase.__annotations__.items()}
