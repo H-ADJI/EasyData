26dab600b1eed424e8d35227014ec7e5636125d0
khalil <khalilhad9@gmail.com>
2022-09-21 11:30:21 +0100

    added the project endpoint router

--- nsa/api/router.py
+++ nsa/api/router.py
@@ -1,7 +1,9 @@
-from nsa.api.routes import authentication
+from nsa.api.routes import authentication, project
 from fastapi import APIRouter
 
 router = APIRouter()
 
 router.include_router(authentication.router, prefix="/user",
+                      tags=["Authentication"])
+router.include_router(project.router, prefix="/project",
                       tags=["Authentication"])
