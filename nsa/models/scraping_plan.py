fe6a45134a97bdea622b6d6ebfae344568cc5c83
khalil <khalilhad9@gmail.com>
2022-09-21 16:38:33 +0100

    created pydantic models for parsing input/ output scraping_plan cruds

---
+++ nsa/models/scraping_plan.py
@@ -0,0 +1,33 @@
+'''
+File: scraping_plan.py
+File Created: Wednesday, 21st September 2022 4:18:16 pm
+Author: KHALIL HADJI 
+-----
+Copyright:  HENCEFORTH 2022
+'''
+from pydantic import BaseModel
+from typing import List, Union, Optional
+from beanie import PydanticObjectId
+from pydantic import validator
+
+
+class Scraping_planBase(BaseModel):
+    title: str
+    website: str
+    plan: dict
+
+
+class Scraping_plan_read(Scraping_planBase):
+    owner_id:  PydanticObjectId
+
+    @validator('plan')
+    def must_validate_schema(cls, plan):
+        # just testing validator decorator will implement the schema validation here later
+        if "description" not in plan:
+            raise ValueError("Must contain a description buddy")
+        return plan 
+
+
+class Scraping_plan_update(Scraping_planBase):
+    __annotations__ = {k: Optional[v]
+                       for k, v in Scraping_planBase.__annotations__.items()}
