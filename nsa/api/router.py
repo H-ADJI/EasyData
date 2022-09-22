e2a7e5c92d91ee705e24999d075bba5dd473c6f4
khalil <khalilhad9@gmail.com>
2022-09-22 16:12:05 +0100

    added schedling routes

--- nsa/api/router.py
+++ nsa/api/router.py
@@ -1,4 +1,4 @@
-from nsa.api.routes import authentication, project, scraping_plan
+from nsa.api.routes import authentication, project, scraping_plan, scheduling
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -9,3 +9,5 @@ router.include_router(project.router, prefix="/project",
                       tags=["Projects"])
 router.include_router(scraping_plan.router, prefix="/plan",
                       tags=["Plans"])
+router.include_router(scheduling.router, prefix="/job",
+                      tags=["Scraping Jobs"])
