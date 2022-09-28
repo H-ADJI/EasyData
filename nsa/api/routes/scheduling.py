40c101122677aa2c4c1e5e1fa906b1456b0b95b9
khalil <khalilhad9@gmail.com>
2022-09-28 11:52:41 +0100

    refactored db models names

--- nsa/api/routes/scheduling.py
+++ nsa/api/routes/scheduling.py
@@ -5,38 +5,49 @@ Author: KHALIL HADJI
 -----
 Copyright:  HENCEFORTH 2022
 '''
-from nsa.models.scheduling import SchedulingBase, Scheduling_read, Scheduling_update, Scheduling_write
+from datetime import datetime
+from nsa.models.scheduling import Interval_trigger, SchedulingBase, Scheduling_read, Scheduling_update, Scheduling_write, Exact_date_trigger
 from nsa.utils.utils import none_remover
-from nsa.database.models import Scheduling, User
+from nsa.database.models import JobScheduling, User
 from nsa.api.routes.authentication import current_user
 from fastapi import Depends, APIRouter, status, HTTPException
 from beanie.exceptions import DocumentNotFound
 from beanie.operators import And
 from beanie import PydanticObjectId
 
-from typing import List
+from typing import List, Optional, Union
 
 router = APIRouter()
 
 
+def compute_next_run(interval: Optional[Interval_trigger], exact_date: Optional[Exact_date_trigger]):
+    pass
+
+
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=Scheduling_read)
 async def schedule_job(job: Scheduling_write, user: User = Depends(current_user)):
-    new_job: Scheduling = Scheduling(owner_id=user.id, **job.dict())
-    await new_job.insert()
+    if job.interval:
+        next_run = compute_next_run(interval=job.interval)
+    elif job.date:
+        next_run = compute_next_run(exact_date=job.date)
 
+    new_job: JobScheduling = JobScheduling(
+        owner_id=user.id, next_run=next_run, **job.dict())
+    await new_job.insert()
     return new_job.dict()
 
 
 @router.get("", status_code=status.HTTP_200_OK, response_model=List[Scheduling_read])
 async def get_all_scheduled_jobs(user: User = Depends(current_user)):
-    all_scheduled_jobs = Scheduling.find(Scheduling.owner_id == user.id)
+    all_scheduled_jobs = JobScheduling.find(
+        JobScheduling.owner_id == user.id)
     all_scheduled_jobs_list = await all_scheduled_jobs.to_list()
     return all_scheduled_jobs_list
 
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=Scheduling_read)
 async def get_one_scheduled_job(id: PydanticObjectId, user: User = Depends(current_user)):
-    job: Scheduling = await Scheduling.find_one(And({Scheduling.id: id}, {Scheduling.owner_id: user.id}))
+    job: JobScheduling = await JobScheduling.find_one(And({JobScheduling.id: id}, {JobScheduling.owner_id: user.id}))
     if job == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Could not find the following job : {id}")
@@ -45,7 +56,7 @@ async def get_one_scheduled_job(id: PydanticObjectId, user: User = Depends(curre
 
 @router.delete("/{id}", status_code=status.HTTP_200_OK, response_model=Scheduling_read)
 async def delete_scheduled_job(id: PydanticObjectId, user: User = Depends(current_user)):
-    job = await Scheduling.find_one(And({Scheduling.id: id}, {Scheduling.owner_id: user.id}))
+    job = await JobScheduling.find_one(And({JobScheduling.id: id}, {JobScheduling.owner_id: user.id}))
     if job == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Could not find the following job : {id}")
@@ -55,13 +66,13 @@ async def delete_scheduled_job(id: PydanticObjectId, user: User = Depends(curren
 
 @router.patch("/{id}", status_code=status.HTTP_200_OK, response_model=Scheduling_read)
 async def update_scheduled_job(id: PydanticObjectId, job_update: Scheduling_update, user: User = Depends(current_user)):
-    old_job = await Scheduling.find_one(And({Scheduling.id: id}, {Scheduling.owner_id: user.id}))
+    old_job = await JobScheduling.find_one(And({JobScheduling.id: id}, {JobScheduling.owner_id: user.id}))
     if old_job == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Could not find the following job : {id}")
     job_update_dict = none_remover(job_update)
     merged_attrs = {**old_job.dict(), **job_update_dict}
-    new_job = Scheduling(**merged_attrs)
+    new_job = JobScheduling(**merged_attrs)
     try:
         await new_job.replace()
     except DocumentNotFound:
--- nsa/api/routes/scraping_plan.py
+++ nsa/api/routes/scraping_plan.py
@@ -6,7 +6,7 @@ Author: KHALIL HADJI
 Copyright:  HENCEFORTH 2022
 '''
 from nsa.models.scraping_plan import Scraping_plan_read, Scraping_plan_update, Scraping_plan_write
-from nsa.database.models import Scraping_plan, User
+from nsa.database.models import ScrapingPlan, User
 from nsa.api.routes.authentication import current_user
 from fastapi import Depends, APIRouter, status, HTTPException
 from beanie.exceptions import DocumentNotFound
@@ -20,7 +20,7 @@ router = APIRouter()
 
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=Scraping_plan_read)
 async def create_plan(scraping_plan: Scraping_plan_write, user: User = Depends(current_user)):
-    new_plan: Scraping_plan = Scraping_plan(
+    new_plan: ScrapingPlan = ScrapingPlan(
         owner_id=user.id, **scraping_plan.dict())
     await new_plan.insert()
 
@@ -29,14 +29,14 @@ async def create_plan(scraping_plan: Scraping_plan_write, user: User = Depends(c
 
 @router.get("", status_code=status.HTTP_200_OK, response_model=List[Scraping_plan_read])
 async def get_all_plans(user: User = Depends(current_user)):
-    all_plans = Scraping_plan.find(Scraping_plan.owner_id == user.id)
+    all_plans = ScrapingPlan.find(ScrapingPlan.owner_id == user.id)
     all_plans_list = await all_plans.to_list()
     return all_plans_list
 
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=Scraping_plan_read)
 async def get_one_plan(id: PydanticObjectId, user: User = Depends(current_user)):
-    plan = await Scraping_plan.find_one(And({Scraping_plan.id: id}, {Scraping_plan.owner_id: user.id}))
+    plan = await ScrapingPlan.find_one(And({ScrapingPlan.id: id}, {ScrapingPlan.owner_id: user.id}))
     if plan == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Could not find the following plan : {id}")
@@ -45,7 +45,7 @@ async def get_one_plan(id: PydanticObjectId, user: User = Depends(current_user))
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_plan(id: PydanticObjectId, user: User = Depends(current_user)):
-    plan = await Scraping_plan.find_one(And({Scraping_plan.id: id}, {Scraping_plan.owner_id: user.id}))
+    plan = await ScrapingPlan.find_one(And({ScrapingPlan.id: id}, {ScrapingPlan.owner_id: user.id}))
     if plan == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Could not find the following plan : {id}")
@@ -55,12 +55,12 @@ async def delete_plan(id: PydanticObjectId, user: User = Depends(current_user)):
 
 @router.patch("/{id}", status_code=status.HTTP_201_CREATED, response_model=Scraping_plan_read)
 async def update_plan(id: PydanticObjectId, plan_updates: Scraping_plan_update, user: User = Depends(current_user)):
-    old_plan = await Scraping_plan.find_one(And({Scraping_plan.id: id}, {Scraping_plan.owner_id: user.id}))
+    old_plan = await ScrapingPlan.find_one(And({ScrapingPlan.id: id}, {ScrapingPlan.owner_id: user.id}))
     if old_plan == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Could not find the following plan : {id}")
     merged_attrs = {**old_plan.dict(), **plan_updates.dict()}
-    new_plan = Scraping_plan(**merged_attrs)
+    new_plan = ScrapingPlan(**merged_attrs)
     try:
         await new_plan.replace()
     except DocumentNotFound:
