799da2fa8177755799c231f58473c00b435a3a43
khalil <khalilhad9@gmail.com>
2022-09-29 15:21:24 +0100

    added enumeration for job status

---
+++ nsa/constants/enums.py
@@ -0,0 +1,15 @@
+'''
+File: enums.py
+File Created: Thursday, 29th September 2022 2:56:51 pm
+Author: KHALIL HADJI 
+-----
+Copyright:  HENCEFORTH 2022
+'''
+from enum import Enum
+
+
+class SchedulingJobStatus(str, Enum):
+    WAITING = "WAITING"
+    DONE = "DONE"
+    FAILED = "FAILED"
+    RECCURING = "RECCURING"
