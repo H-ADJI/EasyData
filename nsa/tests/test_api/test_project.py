cd9165c59feb110bd8537a8433ad433dbed0cc02
khalil <khalilhad9@gmail.com>
2022-10-31 12:03:01 +0100

    added tests for projects crud operation

--- nsa/tests/test_api/test_project.py
+++ nsa/tests/test_api/test_project.py
@@ -5,30 +5,43 @@ Author: KHALIL HADJI
 -----
 Copyright:  HENCEFORTH 2022
 '''
-from fastapi.testclient import TestClient
-from nsa.main import app
-from nsa.models.user import UserRead
+import pytest
+from fastapi import status
+from nsa.tests.conftest import test_client, verified_user, auth_headers, project_data, created_project
 from nsa.database.models import User
-from nsa.tests.conftest import test_db_session
-test_client = TestClient(app)
+from nsa.models import project
+from httpx import AsyncClient, Response
 
 
-def test_read_main():
+async def test_projects_ressource_isprotected(test_client: AsyncClient, project_data: dict):
 
-    response = test_client.get("/")
-    assert response.status_code == 401
-    assert response.json() == {"detail": "Unauthorized"}
+    response: Response = await test_client.post("project", json=project_data)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-def test_create_user(test_db_session):
-    payload = {
-        "email": "test@example.com",
-        "password": "test_password",
-        "first_name": "test",
-        "last_name": "test"
-    }
-    # response = test_client.post("/api/user/register", json=payload)
-    # assert response.status_code == 201
-    # user = UserRead(**response.json())
-    # print(response.json())
-    # assert user.email == payload.get("email")
+async def test_create_project(test_client: AsyncClient, project_data: dict, auth_headers: dict):
+
+    response: Response = await test_client.post("project", json=project_data, headers=auth_headers)
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+async def test_get_project(test_client: AsyncClient, project_data: dict, auth_headers: dict, created_project: project.Project_read):
+
+    response: Response = await test_client.get(f"project/{created_project.id}", headers=auth_headers)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()
+
+
+async def test_update_project(test_client: AsyncClient, auth_headers: dict, created_project: project.Project_read):
+    updated_field = {"title": "udpated title"}
+    response: Response = await test_client.patch(f"project/{created_project.id}", json=updated_field, headers=auth_headers)
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json().get("title") == updated_field.get("title")
+
+
+async def test_delete_project(test_client: AsyncClient,auth_headers: dict, created_project: project.Project_read):
+
+    response: Response = await test_client.delete(f"project/{created_project.id}", headers=auth_headers)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    response: Response = await test_client.get(f"project/{created_project.id}", headers=auth_headers)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
