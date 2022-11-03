2770f577af10b1adce958c248602a27de405eda6
khalil <khalilhad9@gmail.com>
2022-11-03 14:33:03 +0100

    added fixture for schedulign and celery tests

--- nsa/tests/conftest.py
+++ nsa/tests/conftest.py
@@ -8,28 +8,53 @@ Copyright:  HENCEFORTH 2022
 from datetime import datetime, timedelta
 import motor.motor_asyncio
 from nsa.configs.configs import env_settings
-import nest_asyncio
+from nsa.constants.enums import SchedulingJobStatus
 import asyncio
-from nsa.database.models import Project, User, ScrapingPlan, JobScheduling
+from nsa.database.models import Project, User, ScrapingPlan, JobScheduling, JobExecutionHistory
 from nsa.models import project, scraping_plan, scheduling
 from beanie import init_beanie
 from fastapi import FastAPI
 from typing import Generator
-from nsa.database.database import db
 from httpx import AsyncClient, Response
 import pytest
 from nsa.main import get_app
-from nsa.api.routes.authentication import create_super_user
-from nsa.models.user import UserCreate
 import pytz
 from beanie import PydanticObjectId
+from freezegun import freeze_time
+from nsa.services.utils import construct_aio_threading, destruct_aio_threading
+from nsa.services.base_task import BaseTask
+from nsa.services.async_sync import async_to_sync
+
+FROZEN_TIME = "2022-07-15T12:30:00"
 
 BASE_URL: str = 'http://localhost:8000/api'
-nest_asyncio.apply()
 
 
-@pytest.fixture(scope="session")
-async def mongo_is_ready():
+
+@pytest.fixture()
+async def celery_mongo_is_ready(aio_thread):
+    DB_NAME = env_settings.MONGO_TEST_DB_NAME
+    MONGO_USER = env_settings.MONGO_INITDB_ROOT_USERNAME
+    MONGO_PASSWORD = env_settings.MONGO_INITDB_ROOT_PASSWORD
+    MONGO_HOST = env_settings.MONGO_HOST
+    MONGO_PORT = env_settings.MONGO_PORT
+    DATABASE_URL = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}"
+    client: motor.motor_asyncio.core.AgnosticClient = motor.motor_asyncio.AsyncIOMotorClient(
+        DATABASE_URL, uuidRepresentation="standard"
+    )
+    db = client[DB_NAME]
+    async_to_sync(aio_thread=BaseTask.aio_thread, coroutine=init_beanie(
+        database=db,
+        document_models=[Project, User, ScrapingPlan,
+                         JobScheduling, JobExecutionHistory],
+    ))
+    yield
+    async_to_sync(aio_thread=BaseTask.aio_thread,
+                  coroutine=client.drop_database(db))
+
+
+@pytest.fixture()
+async def mongo_is_ready(aio_thread):
     DB_NAME = env_settings.MONGO_TEST_DB_NAME
     MONGO_USER = env_settings.MONGO_INITDB_ROOT_USERNAME
     MONGO_PASSWORD = env_settings.MONGO_INITDB_ROOT_PASSWORD
@@ -42,20 +67,21 @@ async def mongo_is_ready():
     db = client[DB_NAME]
     await init_beanie(
         database=db,
-        document_models=[Project, User, ScrapingPlan, JobScheduling],
+        document_models=[Project, User, ScrapingPlan,
+                         JobScheduling, JobExecutionHistory],
     )
     yield
     await client.drop_database(db)
 
 
-@pytest.fixture(scope='function')
+@ pytest.fixture(scope='function')
 def app(mongo_is_ready) -> FastAPI:
     """Return instance of the fast API instance
     """
     return get_app(testing=True)
 
 
-@pytest.fixture(scope="session")
+@ pytest.fixture(scope="session")
 def event_loop():
     # get the current event loop
     loop = asyncio.get_event_loop()
@@ -63,7 +89,7 @@ def event_loop():
     yield loop
 
 
-@pytest.fixture
+@ pytest.fixture
 async def test_client(app: FastAPI) -> Generator:
     """Return fast api test client
     """
@@ -71,7 +97,7 @@ async def test_client(app: FastAPI) -> Generator:
         yield c
 
 
-@pytest.fixture
+@ pytest.fixture
 async def verified_user(test_client: AsyncClient) -> Generator:
     """Create a dummy user
     """
@@ -85,7 +111,7 @@ async def verified_user(test_client: AsyncClient) -> Generator:
     yield user
 
 
-@pytest.fixture
+@ pytest.fixture
 async def auth_headers(test_client: AsyncClient, verified_user: dict):
     """ get auth token """
     data = {"username": f"{verified_user.get('email')}",
@@ -102,7 +128,7 @@ async def auth_headers(test_client: AsyncClient, verified_user: dict):
     yield headers
 
 
-@pytest.fixture
+@ pytest.fixture
 def project_data():
     project_data = {
         "description": "some description",
@@ -154,7 +180,6 @@ async def created_scraping_plan(test_client, auth_headers, valid_scraping_plan):
 @pytest.fixture
 async def created_job(test_client: AsyncClient, auth_headers, valid_scheduling_interval):
     response: Response = await test_client.post("job", json=valid_scheduling_interval, headers=auth_headers)
-
     yield scheduling.Scheduling_read(**response.json())
 
 
@@ -258,3 +283,79 @@ def soon_scheduling_exact_date():
         "input_data": {}
     }
     yield job_scheduling
+
+
+
+def generate_date(date: datetime, offset: int):
+    return date.replace(tzinfo=None) + timedelta(minutes=offset)
+
+
+@ pytest.fixture(scope="session")
+def aio_thread():
+    construct_aio_threading(aio_thread=BaseTask.aio_thread)
+    yield
+    destruct_aio_threading(aio_thread=BaseTask.aio_thread)
+
+
+@ pytest.fixture()
+async def insert_exact_date_jobs():
+    with freeze_time(FROZEN_TIME):
+        date = datetime.now(tz=pytz.timezone("Africa/Casablanca"))
+
+        jobs = [
+            JobScheduling(** {
+                "id": PydanticObjectId(f"555f9a89538ace78a21e01{i + 1}a"),
+                "owner_id": PydanticObjectId("666f9a89538ace78a21e010a"),
+                "plan_id": PydanticObjectId("777f9a89538ace78a21e010a"),
+                "exact_date": {
+                    # dates would be : 2022-07-15T12:29:00,  2022-07-15T12:30:00 and 2022-07-15T12:31:00
+
+                    "date": generate_date(offset=i, date=date),
+                    "timezone": "Africa/Casablanca"
+                },
+                # next run is automatically set to date
+                "next_run": generate_date(offset=i, date=date),
+                "input_data": {},
+                "status": SchedulingJobStatus.WAITING
+            }) for i in range(-1, 2)
+        ]
+        async_to_sync(aio_thread=BaseTask.aio_thread,
+                      coroutine=JobScheduling.insert_many(jobs))
+        yield
+        # here delete the jobs
+        for job in jobs:
+            async_to_sync(aio_thread=BaseTask.aio_thread,
+                          coroutine=JobScheduling.find(JobScheduling.id == job.id).delete())
+
+
+@ pytest.fixture()
+async def insert_reoccurent_jobs():
+    with freeze_time(FROZEN_TIME):
+        date = datetime.now(tz=pytz.timezone("Africa/Casablanca"))
+        jobs = [
+            JobScheduling(**{
+                "id": PydanticObjectId(f"555f9a89538ace78a21e01{i+5}b"),
+                "owner_id": PydanticObjectId("666f9a89538ace78a21e010a"),
+                "plan_id": PydanticObjectId("777f9a89538ace78a21e010a"),
+                "interval": {
+                    "minutes": env_settings.MIN_ACCEPTED_INTERVAL,
+                    # start dates would be : 2022-07-15T12:28:00, 2022-07-15T12:29:00,  2022-07-15T12:30:00 and 2022-07-15T12:31:00
+                    "start_date": generate_date(date=date, offset=i),
+                    # end dates would be : 2022-07-15T12:30:00, 2022-07-15T12:31:00,  2022-07-15T12:32:00 and 2022-07-15T12:33:00
+                    "end_date": generate_date(date=date, offset=2+i),
+                    "timezone": "Africa/Casablanca"
+                },
+                # next run is automatically set to start_date
+                "next_run":  generate_date(date=date, offset=i),
+                "input_data": {},
+                "status": SchedulingJobStatus.WAITING
+            })
+            for i in range(-2, 2)
+        ]
+        async_to_sync(aio_thread=BaseTask.aio_thread,
+                      coroutine=JobScheduling.insert_many(jobs))
+        yield 
+        # here delete the jobs
+        for job in jobs:
+            async_to_sync(aio_thread=BaseTask.aio_thread,
+                          coroutine=JobScheduling.find(JobScheduling.id == job.id).delete())
