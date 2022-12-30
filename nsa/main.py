78e537309273fedcc46ad9c1d58de98f21ba718c
khalil <khalilhad9@gmail.com>
2022-12-30 10:40:09 +0100

    allow CORS

--- nsa/main.py
+++ nsa/main.py
@@ -9,15 +9,22 @@ from nsa.api.router import router
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from nsa.configs.event_handlers import startup_handler, shutdown_handler
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def get_app(testing: bool = False) -> FastAPI:
+    origins = [
+        "http://localhost:4200"
+    ]
+
     app = FastAPI(title="NSA", version="0.0.1")
     app.include_router(router)
-    if not testing :
-        app.add_event_handler(event_type="startup", func=startup_handler(app=app))
+    if not testing:
+        app.add_event_handler(event_type="startup",
+                              func=startup_handler(app=app))
         app.add_event_handler(event_type="shutdown",
-                          func=shutdown_handler(app=app))
+                              func=shutdown_handler(app=app))
+        app.add_middleware(CORSMiddleware, allow_origins=origins)
     return app
 
 
