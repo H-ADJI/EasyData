61557632d2ae669a3519a15d789bc639a8601941
H-ADJI <khalil.o.3.lach@gmail.com>
2023-01-31 18:23:10 +0100

    added template for env variables

--- easydata/constants/constants.py
+++ easydata/constants/constants.py
@@ -5,5 +5,5 @@ Author: KHALIL HADJI
 -----
 Copyright:  H-adji 2022
 '''
-PATH_SCRAPING_PLAN_SCHEMA = "nsa/services/validation/plan_schema.json"
+PATH_SCRAPING_PLAN_SCHEMA = "easydata/services/validation/plan_schema.json"
 WORKFLOWS_LIST_INPUT_SEPARATOR = "|*|"
